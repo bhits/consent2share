685cae91cb6aad4dd31016159c2649a5825796a5
CindyLiFei <cindy.li@feisystems.com>
2016-04-04 03:12:28 -0400

    Update docker compose and add build-all.sh.
    # 8418 -  Set up development environment (MHC Apps) using one command and development workflow.

---
+++ build-all.sh
@@ -0,0 +1,15 @@
+#!run in git bash
+
+cd admin-portal-ui/server;                          mvn clean package -Pdocker docker:build; cd -; 
+cd discovery-server/discovery-server;               mvn clean package docker:build; cd -; 
+cd dss-api/dss;                             		mvn clean package docker:build; cd -; 
+cd edge-server/edge-server;                  		mvn clean package docker:build; cd -; 
+cd patient-portal-ui/server;                        mvn clean package -Pdocker docker:build; cd -; 
+cd patient-user-api/patient-user;                   mvn clean package docker:build; cd -; 
+cd pcm-api/pcm;                             		mvn clean package docker:build; cd -; 
+cd phr-api/phr;                  					mvn clean package docker:build; cd -; 
+cd pls-api/pls/web;                             	mvn clean package docker:build; cd -; 
+cd registration-api/registration;                  	mvn clean package docker:build; cd -; 
+cd try-policy-api/tryPolicy;                        mvn clean package docker:build; cd -; 
+cd iexhub/iexhub;                                   mvn clean package docker:build; cd -;;                       
+cd uaa;											    gradlew clean install; cp uaa/build/libs/*.war docker/uaa.war; cd docker; docker build -t uaa .;
