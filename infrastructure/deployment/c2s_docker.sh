73a8b2128e40f7246198d95bf2a2246d1d0f1bb1
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-10-14 09:54:03 -0400

    Set edge server https configuration and database environment variables
    #11140 - As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share

---
+++ infrastructure/deployment/c2s_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export UAA_DB_PASSWORD=admin
+export PCM_DB_PASSWORD=admin
+export PLS_DB_PASSWORD=admin
+export PHR_DB_PASSWORD=admin
+export PATIENT_USER_DB_PASSWORD=admin
+export AUDIT_DB_PASSWORD=admin
+#Database server details in case of two Server setup
+#export C2S_BASE_PATH=/usr/local
+#export C2S_DB_HOST=bhitsqadblnx01
