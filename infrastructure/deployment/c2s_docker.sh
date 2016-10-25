6604b2e9357c7319f862d654f4d360489ca887e5
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-10-25 06:18:13 -0400

    Add logback-audit and HIE connection variables
    #11423 -As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/deployment/c2s_docker.sh
+++ infrastructure/deployment/c2s_docker.sh
@@ -17,3 +17,7 @@ export UAA_SMTP_PORT=25
 export UAA_SMTP_USER=bhits.mail
 export UAA_SMTP_PASSWORD="M@il4Bhit$"
 export UAA_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvQlpuZSYUKHOkGCW4AwWKWgf/jukU0vrZQ/6vfAU8tmCYoPJfqsqA9zgJbvj8UPQ/sjmY7txjpAw6qTz3/yW4lSkkZyz25eSy9+/vUY/WSghHxjYWnhaGDBD5Qgs7TmzotRvia33rZxtcq1Osylt/WufFc0RClPwEqLQCISYMiC4TnccE+wkFaDAwfgVIzEoHhiOeItRvgghYEHHDuFi1uYNo5XvyYop5ulT4HZuS7wccm2jCuzHAeJULxN7epc7juib93OsLyEWfqfk/Jif nW9n1Kr7Ph6teDXTsZ/jFM/nFFfNpJeNfW2bgOG/3fJWf8zGhbTx426uznn3iG0wWwIDAQAB-----END PUBLIC KEY-----"
+export HIE_CONNECTION_ENABLE=false
+#logback-audit variables
+export AUTO_SCAN=false
+export SCAN_PERIOD="30 seconds"
