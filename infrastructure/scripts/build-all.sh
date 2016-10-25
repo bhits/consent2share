f3f040760517bc44ef978031626e2cd7e695e2a9
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-10-25 06:16:07 -0400

    Sort in alphabetical order
    #11423 -As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/scripts/build-all.sh
+++ infrastructure/scripts/build-all.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-cd ..;
-cd ..;
-cd admin-portal-ui/server;                          mvn clean package -Pdocker docker:build; cd -; 
+cd ../../..;
+cd admin-portal-ui/server;                          mvn clean package -Pdocker docker:build; cd -;
 cd context-handler/context-handler;                 mvn clean package docker:build; cd -;
 cd discovery-server/discovery-server;               mvn clean package docker:build; cd -;
 cd document-validator/document-validator;           mvn clean package; cd -;
@@ -10,13 +9,14 @@ cd document-validator/document-validator/document-validator-ccda-r2;           m
 cd dss-api/dss;                             		mvn clean package docker:build; cd -;
 cd edge-server/edge-server;                  		mvn clean package docker:build; cd -;
 cd iexhub-fork/iexhub;                              mvn clean package docker:build; cd -;
-cd pep-api/pep;                                     mvn clean package docker:build; cd -;
+cd logback-audit;                                   mvn clean install; cd -;cd logback-audit/audit-server-generator/logback-audit-server;mvn clean package docker:build;cd -;
 cd patient-portal-ui/server;                        mvn clean package -Pdocker docker:build; cd -;
 cd patient-user-api/patient-user;                   mvn clean package docker:build; cd -;
 cd pcm-api/pcm;                             		mvn clean package docker:build; cd -;
+cd pep-api/pep;                                     mvn clean package docker:build; cd -;
 cd phr-api/phr;                  					mvn clean package docker:build; cd -;
 cd pls-api/pls;                             	    mvn clean package; cd -;
 cd pls-api/pls/web;                             	mvn clean package docker:build; cd -;
 cd registration-api/registration;                  	mvn clean package docker:build; cd -;
 cd try-policy-api/tryPolicy;                        mvn clean package docker:build; cd -;
-cd uaa;											    ./gradlew clean install; cp uaa/build/libs/*.war docker/uaa.war; cp config-template/uaa.yml docker/uaa.yml;cd docker; docker build -t bhits/uaa .; rm uaa.war; rm uaa.yml;
+cd uaa;											    ./gradlew clean install; cp uaa/build/libs/*.war docker/uaa.war; cp config-template/uaa.yml docker/uaa.yml;cd docker; docker build -t bhits/uaa .; rm uaa.war; rm uaa.yml; cd ../..;
