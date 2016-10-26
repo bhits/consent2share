babe3f9595d46d6cd956e47d76abb8b834b98f6e
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-10-26 06:56:38 -0400

    Correct docker push commands for all images
    #11423 -As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/scripts/publish-dockerhub.sh
+++ infrastructure/scripts/publish-dockerhub.sh
@@ -19,25 +19,24 @@ docker tag bhits/try-policy bhits/try-policy:1.13.0;
 docker tag bhits/uaa bhits/uaa:3.4.1-01;
 
 
-docker push bhits/admin-portal-ui bhits/admin-portal-ui:0.14.0;
-docker push bhits/context-handler bhits/context-handler:1.8.0;
-docker push bhits/discovery-server bhits/discovery-server:0.8.0;
-docker push bhits/document-validator-ccda-r1 bhits/document-validator-ccda-r1:0.9.0;
-docker push bhits/document-validator-ccda-r2 bhits/document-validator-ccda-r2:0.9.0;
-docker push bhits/dss bhits/dss:1.13.0;
-docker push bhits/edge-server bhits/edge-server:0.11.0;
-docker push bhits/iexhub-fork bhits/iexhub-fork:0.3.0;
+docker push bhits/admin-portal-ui:0.14.0;
+docker push bhits/context-handler:1.8.0;
+docker push bhits/discovery-server:0.8.0;
+docker push bhits/document-validator-ccda-r1:0.9.0;
+docker push bhits/document-validator-ccda-r2:0.9.0;
+docker push bhits/dss:1.13.0;
+docker push bhits/edge-server:0.11.0;
+docker push bhits/iexhub-fork:0.3.0;
 docker push bhits/logback-audit-server:0.6.1
-docker push bhits/patient-registration bhits/patient-registration:1.15.0;
-docker push bhits/patient-user bhits/patient-user:0.12.0;
-docker push bhits/pcm bhits/pcm:1.18.0;
-docker push bhits/pep bhits/pep:1.7.0;
-docker push bhits/phr bhits/phr:1.18.0;
-docker push bhits/pls bhits/pls:1.11.0;
-docker push bhits/pp-ui bhits/pp-ui:0.20.0;
-docker push bhits/try-policy bhits/try-policy:1.13.0;
-docker push bhits/uaa bhits/uaa:3.4.1-01;
-
+docker push bhits/patient-registration:1.15.0;
+docker push bhits/patient-user:0.12.0;
+docker push bhits/pcm:1.18.0;
+docker push bhits/pep:1.7.0;
+docker push bhits/phr:1.18.0;
+docker push bhits/pls:1.11.0;
+docker push bhits/pp-ui:0.20.0;
+docker push bhits/try-policy:1.13.0;
+docker push bhits/uaa:3.4.1-01;
 
 
 
