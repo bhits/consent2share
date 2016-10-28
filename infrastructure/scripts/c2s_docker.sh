eecba7264243b074037d3e2ed06f504efab3f841
burcakulug <burcak.ulug@feisystems.com>
2016-10-28 14:21:15 -0400

    Eliminate duplicate c2s_docker.sh files
    #11423:As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/scripts/c2s_docker.sh
+++ infrastructure/scripts/c2s_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#Db  details
+# Database details
+# This variable is only required in 'two-servers' deployment scenario, so it must be uncommented and configured accordingly in 'two-servers' deployment.
+#export C2S_DB_HOST=your_db_server_host
 export UAA_DB_PASSWORD=admin
 export PCM_DB_PASSWORD=admin
 export PLS_DB_PASSWORD=admin
@@ -7,14 +9,14 @@ export PHR_DB_PASSWORD=admin
 export PATIENT_USER_DB_PASSWORD=admin
 export AUDIT_DB_PASSWORD=admin
 export C2S_BASE_PATH=/usr/local
-# Edge server details
-export C2S_APP_HOST=bhitsqaapplnx01
+# Edge Server details
+export C2S_APP_HOST=your_app_server_host
 export C2S_APP_PORT=80
 # SMTP details
-export UAA_SMTP_HOST=mail.feisystems.com
-export UAA_SMTP_PORT=25
-export UAA_SMTP_USER=bhits.mail
-export UAA_SMTP_PASSWORD="M@il4Bhit$"
+export UAA_SMTP_HOST=mail_host
+export UAA_SMTP_PORT=mail_port
+export UAA_SMTP_USER=mail_username
+export UAA_SMTP_PASSWORD=mail_password
 export UAA_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0m59l2u9iDnMbrXHfqkOrn2dVQ3vfBJqcDuFUK03d+1PZGbVlNCqnkpIJ8syFppW8ljnWweP7+LiWpRoz0I7fYb3d8TjhV86Y997Fl4DBrxgM6KTJOuE/uxnoDhZQ14LgOU2ckXjOzOdTsnGMKQBLCl0vpcXBtFLMaSbpv1ozi8h7DJyVZ6EnFQZUWGdgTMhDrmqevfx95U/16c5WBDOkqwIn7Glry9n9Suxygbf8g5AzpWcusZgDLIIZ7JTUldBb8qU2a0Dl4mvLZOn4wPojfj9Cw2QICsc5+Pwf21fP+hzf+1WSRHbnYv8uanRO0gZ8ekGaghM/2H6gqJbo2nIJwIDAQAB-----END PUBLIC KEY-----"
 export HIE_CONNECTION_ENABLE=false
 #logback-audit variables
