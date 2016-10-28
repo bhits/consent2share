72d724572bb88acdf49731ed6b4368abe939b200
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2016-10-28 14:46:55 -0400

    update admin-portal to latest version and remove iexhub-fork references
    #11423 -As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/scripts/publish-dockerhub.sh
+++ infrastructure/scripts/publish-dockerhub.sh
@@ -6,8 +6,7 @@ docker tag bhits/document-validator-ccda-r1 bhits/document-validator-ccda-r1:0.9
 docker tag bhits/document-validator-ccda-r2 bhits/document-validator-ccda-r2:0.9.0;
 docker tag bhits/dss bhits/dss:1.13.0;
 docker tag bhits/edge-server bhits/edge-server:0.11.0;
-#docker tag bhits/iexhub-fork bhits/iexhub-fork:0.3.0;
-docker tag bhits/iexhub-fork bhits/iexhub:0.3.0;
+docker tag bhits/iexhub bhits/iexhub:0.3.0;
 docker tag bhits/logback-audit-server bhits/logback-audit-server:0.6.1
 docker tag bhits/patient-registration bhits/patient-registration:1.15.0;
 docker tag bhits/patient-user bhits/patient-user:0.12.0;
@@ -27,7 +26,6 @@ docker push bhits/document-validator-ccda-r1:0.9.0;
 docker push bhits/document-validator-ccda-r2:0.9.0;
 docker push bhits/dss:1.13.0;
 docker push bhits/edge-server:0.11.0;
-#docker push bhits/iexhub-fork:0.3.0;
 docker push bhits/iexhub:0.3.0;
 docker push bhits/logback-audit-server:0.6.1
 docker push bhits/patient-registration:1.15.0;
@@ -49,7 +47,6 @@ docker push bhits/document-validator-ccda-r1;
 docker push bhits/document-validator-ccda-r2;
 docker push bhits/dss;
 docker push bhits/edge-server;
-#docker push bhits/iexhub-fork;
 docker push bhits/iexhub;
 docker push bhits/logback-audit-server
 docker push bhits/patient-user;
