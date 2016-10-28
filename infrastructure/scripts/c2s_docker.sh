395621f6084622fc5a9a7a95bfe8476b707eebc8
burcakulug <burcak.ulug@feisystems.com>
2016-10-28 14:45:30 -0400

    Change default for AUTO_SCAN and place holders for mail properties
    #11423:As a community member (HIE DevOps), I can reference a Consent2Share deployment guide in order to deploy and host Consent2Share (Part 2)

--- infrastructure/scripts/c2s_docker.sh
+++ infrastructure/scripts/c2s_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Database details
-# This variable is only required in 'two-servers' deployment scenario, so it must be uncommented and configured accordingly in 'two-servers' deployment.
+# This variable is only required in 'two-servers' deployment scenario, so it MUST be uncommented and configured accordingly in 'two-servers' deployment.
 #export C2S_DB_HOST=your_db_server_host
 export UAA_DB_PASSWORD=admin
 export PCM_DB_PASSWORD=admin
@@ -13,12 +13,12 @@ export C2S_BASE_PATH=/usr/local
 export C2S_APP_HOST=your_app_server_host
 export C2S_APP_PORT=80
 # SMTP details
-export UAA_SMTP_HOST=mail_host
-export UAA_SMTP_PORT=mail_port
-export UAA_SMTP_USER=mail_username
-export UAA_SMTP_PASSWORD=mail_password
+export UAA_SMTP_HOST=your_mail_host
+export UAA_SMTP_PORT=your_mail_port
+export UAA_SMTP_USER=your_mail_username
+export UAA_SMTP_PASSWORD=your_mail_password
 export UAA_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0m59l2u9iDnMbrXHfqkOrn2dVQ3vfBJqcDuFUK03d+1PZGbVlNCqnkpIJ8syFppW8ljnWweP7+LiWpRoz0I7fYb3d8TjhV86Y997Fl4DBrxgM6KTJOuE/uxnoDhZQ14LgOU2ckXjOzOdTsnGMKQBLCl0vpcXBtFLMaSbpv1ozi8h7DJyVZ6EnFQZUWGdgTMhDrmqevfx95U/16c5WBDOkqwIn7Glry9n9Suxygbf8g5AzpWcusZgDLIIZ7JTUldBb8qU2a0Dl4mvLZOn4wPojfj9Cw2QICsc5+Pwf21fP+hzf+1WSRHbnYv8uanRO0gZ8ekGaghM/2H6gqJbo2nIJwIDAQAB-----END PUBLIC KEY-----"
 export HIE_CONNECTION_ENABLE=false
 #logback-audit variables
-export AUTO_SCAN=false
+export AUTO_SCAN=true
 export SCAN_PERIOD="30 seconds"
