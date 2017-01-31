66b4602abefc847ffdc063aaf6e05676964d54f5
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-01-31 13:12:04 -0500

    Remove OAUTH client key details for Admin and PP UI , Add Basic Auth details for edge server and encrypt key for config server.
    #12548 - As a developer, I will have deployed to QA Linux Server and Demo Linux Server 2.1

--- infrastructure/scripts/c2s_docker.sh
+++ infrastructure/scripts/c2s_docker.sh
@@ -17,11 +17,14 @@ export UAA_SMTP_HOST=your_mail_host
 export UAA_SMTP_PORT=your_mail_port
 export UAA_SMTP_USER=your_mail_username
 export UAA_SMTP_PASSWORD=your_mail_password
-export UAA_PUBLIC_KEY="-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0m59l2u9iDnMbrXHfqkOrn2dVQ3vfBJqcDuFUK03d+1PZGbVlNCqnkpIJ8syFppW8ljnWweP7+LiWpRoz0I7fYb3d8TjhV86Y997Fl4DBrxgM6KTJOuE/uxnoDhZQ14LgOU2ckXjOzOdTsnGMKQBLCl0vpcXBtFLMaSbpv1ozi8h7DJyVZ6EnFQZUWGdgTMhDrmqevfx95U/16c5WBDOkqwIn7Glry9n9Suxygbf8g5AzpWcusZgDLIIZ7JTUldBb8qU2a0Dl4mvLZOn4wPojfj9Cw2QICsc5+Pwf21fP+hzf+1WSRHbnYv8uanRO0gZ8ekGaghM/2H6gqJbo2nIJwIDAQAB-----END PUBLIC KEY-----"
-export HIE_CONNECTION_ENABLE=false
 #logback-audit variables
 export AUTO_SCAN=true
 export SCAN_PERIOD="30 seconds"
 #uaa client id keys
 export UAA_ADMIN_UI_SECRET=changeit
 export UAA_PP_UI_SECRET=changeit
+#conofig server variables
+export BASIC_AUTH_USER=your_basic_auth_user
+export BASIC_AUTH_PASSWORD=your_basic_auth_password
+#conofig data variables
+export CONFIG_DATA_ENCRYPT_KEY=your_config_data_encrypt
