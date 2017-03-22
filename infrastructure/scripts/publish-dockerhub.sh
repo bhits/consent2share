700c6e8562fe485b40ffd8b519c429eda4c4e217
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-03-22 09:55:59 -0400

    Update docker images publish details to meet Rl 3.0.0 requirements
    #12893 - As a developer, I will have released C2S version [3.0.0]

--- infrastructure/scripts/publish-dockerhub.sh
+++ infrastructure/scripts/publish-dockerhub.sh
@@ -1,63 +1,32 @@
 #! /bin/bash
-docker tag bhits/admin-portal-ui bhits/admin-portal-ui:0.18.0;
-docker tag bhits/config-server bhits/config-server:0.2.0;
-docker tag bhits/context-handler bhits/context-handler:1.11.0;
-docker tag bhits/discovery-server bhits/discovery-server:0.10.0;
-docker tag bhits/document-validator-ccda-r1 bhits/document-validator-ccda-r1:0.11.0;
-docker tag bhits/document-validator-ccda-r2 bhits/document-validator-ccda-r2:0.11.0;
-docker tag bhits/dss bhits/dss:1.16.0;
-docker tag bhits/edge-server bhits/edge-server:0.14.0;
-docker tag bhits/iexhub bhits/iexhub:0.6.0;
-docker tag bhits/logback-audit-server bhits/logback-audit-server:0.6.1
-docker tag bhits/patient-registration bhits/patient-registration:1.19.0;
-docker tag bhits/patient-user bhits/patient-user:0.15.0;
-docker tag bhits/pcm bhits/pcm:1.22.0;
-docker tag bhits/pep bhits/pep:1.10.0;
-docker tag bhits/phr bhits/phr:1.22.0;
-docker tag bhits/pls bhits/pls:2.1.0;
-docker tag bhits/pp-ui bhits/pp-ui:0.24.0;
-docker tag bhits/try-policy bhits/try-policy:1.16.0;
-docker tag bhits/uaa bhits/uaa:3.4.1-02;
+docker tag bhits/c2s-ui bhits/c2s-ui:0.1.0;
+docker tag bhits/c2s-ui-api bhits/c2s-ui-api:0.1.0;
+docker tag bhits/config-server bhits/config-server:0.3.0;
+docker tag bhits/discovery-server bhits/discovery-server:0.11.0;
+docker tag bhits/edge-server bhits/edge-server:0.15.0;
+docker tag bhits/pcm bhits/pcm:2.0.0;
+docker tag bhits/pls bhits/pls:3.0.0;
+docker tag bhits/uaa bhits/uaa:3.4.1-04;
+docker tag bhits/vss bhits/vss:0.2.0;
 
 #Push docker images to docker hub
-docker push bhits/admin-portal-ui:0.18.0;
-docker push bhits/config-server:0.2.0;
-docker push bhits/context-handler:1.11.0;
-docker push bhits/discovery-server:0.10.0;
-docker push bhits/document-validator-ccda-r1:0.11.0;
-docker push bhits/document-validator-ccda-r2:0.11.0;
-docker push bhits/dss:1.16.0;
-docker push bhits/edge-server:0.14.0;
-docker push bhits/iexhub:0.6.0;
-docker push bhits/logback-audit-server:0.6.1
-docker push bhits/patient-registration:1.19.0;
-docker push bhits/patient-user:0.15.0;
-docker push bhits/pcm:1.22.0;
-docker push bhits/pep:1.10.0;
-docker push bhits/phr:1.22.0;
-docker push bhits/pls:2.1.0;
-docker push bhits/pp-ui:0.24.0;
-docker push bhits/try-policy:1.16.0;
+docker push bhits/c2s-ui:0.1.0;
+docker push bhits/c2s-ui-api:0.1.0;
+docker push bhits/config-server:0.3.0;
+docker push bhits/discovery-server:0.11.0;
+docker push bhits/edge-server:0.15.0;
+docker push bhits/pcm:2.0.0;
+docker push bhits/pls:3.0.0;
 docker push bhits/uaa:3.4.1-02;
+docker push bhits/vss:0.1.0;
 
 
-
-docker push bhits/admin-portal-ui;
+docker push bhits/c2s-ui;
+docker push bhits/c2s-ui-api;
 docker push bhits/config-server;
-docker push bhits/context-handler;
 docker push bhits/discovery-server;
-docker push bhits/document-validator-ccda-r1;
-docker push bhits/document-validator-ccda-r2;
-docker push bhits/dss;
 docker push bhits/edge-server;
-docker push bhits/iexhub;
-docker push bhits/logback-audit-server
-docker push bhits/patient-user;
-docker push bhits/patient-registration;
 docker push bhits/pcm;
-docker push bhits/pep;
-docker push bhits/phr;
 docker push bhits/pls;
-docker push bhits/pp-ui;
-docker push bhits/try-policy;
 docker push bhits/uaa;
+docker push bhits/vss;
