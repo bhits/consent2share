ea97538cc7203b6d39fcafa26756bcf48eb8072d
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-03-22 09:55:04 -0400

    Update env details for one server deployment to meet Rl 3.0.0 requirements
    #12893 - As a developer, I will have released C2S version [3.0.0]

--- infrastructure/scripts/c2s_one_server_env.sh
+++ infrastructure/scripts/c2s_one_server_env.sh
@@ -6,9 +6,6 @@ function defaultConfig() {
     export UAA_DB_PASSWORD=admin
     export PCM_DB_PASSWORD=admin
     export PLS_DB_PASSWORD=admin
-    export PHR_DB_PASSWORD=admin
-    export PATIENT_USER_DB_PASSWORD=admin
-    export AUDIT_DB_PASSWORD=admin
     export C2S_BASE_PATH=/usr/local
     export CONFIG_DATA_GIT_DIR=c2s-config-data
     # Edge Server configuraiton
@@ -18,9 +15,6 @@ function defaultConfig() {
     export UAA_SMTP_PORT=your_mail_port
     export UAA_SMTP_USER=your_mail_username
     export UAA_SMTP_PASSWORD=your_mail_password
-    #logback-audit variables
-    export AUTO_SCAN=true
-    export SCAN_PERIOD="30 seconds"
    }
 
 function oneServerConfig() {
