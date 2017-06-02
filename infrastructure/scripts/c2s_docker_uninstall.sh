92e667700129bf9678e408cc26c9f0187c4d8a34
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-02 12:26:15 -0400

    Add commands to uninstall previous versions of docker
    #13775 - As a developer, I will have implemented the VSS Backend [v3.0]

--- infrastructure/scripts/c2s_docker_uninstall.sh
+++ infrastructure/scripts/c2s_docker_uninstall.sh
@@ -3,15 +3,16 @@ dockerVersion=docker-engine-1.13.0-1.el7.centos
 dockerComposeVersion=1.10.1
 
 sudo su << SudoUser
-    yum -y remove docker \
-              docker-common \
-              container-selinux \
-              docker-selinux \
-              docker-engine
-    rm -rf /var/lib/docker
-    rm /usr/local/bin/docker-compose
-    yum -y remove docker
-    yum -y remove docker-engine
-    rm -rf /var/lib/docker
+       yum -y remove docker \
+                  docker-common \
+                  container-selinux \
+                  docker-selinux \
+                  docker-engine
+
+        rm -rf /var/lib/docker
+
+        rm /usr/local/bin/docker-compose
+
+        rm -rf /etc/docker
 SudoUser
 
