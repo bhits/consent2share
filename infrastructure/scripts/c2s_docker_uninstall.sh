06b029cf7a5a6ab5e1eb80834054e6bb1223fbbb
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-02 13:06:14 -0400

    remove docker-ce install for community version
    #13775 - As a developer, I will have implemented the VSS Backend [v3.0]

--- infrastructure/scripts/c2s_docker_uninstall.sh
+++ infrastructure/scripts/c2s_docker_uninstall.sh
@@ -3,11 +3,15 @@ dockerVersion=docker-engine-1.13.0-1.el7.centos
 dockerComposeVersion=1.10.1
 
 sudo su << SudoUser
+       # Start Docker
+       service docker stop
+
        yum -y remove docker \
                   docker-common \
                   container-selinux \
                   docker-selinux \
-                  docker-engine
+                  docker-engine \
+                  docker-ce
 
         rm -rf /var/lib/docker
 
